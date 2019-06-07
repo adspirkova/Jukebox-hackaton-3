af5f22f5f8a12db86c9258d8e9a703ac24aa2c73
Adela Spirkova <ad.spirkova@gmail.com>
2019-06-07 14:24:13 +0200

    commititng Songs Controller

---
+++ resources/views/forms/select.php
@@ -0,0 +1,5 @@
+        <!-- <select name="songName" id="">
+        <?php foreach ($record as $key => $value)  : ?>
+                <option value="$value -> songName"><?= $record == $value ? 'selected' : '' ?><?= $record -> songName ?></option> 
+            <?php endforeach ;?>
+        </select> -->
