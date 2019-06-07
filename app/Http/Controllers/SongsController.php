223b96b89fbe07a697b2959fb7b3d3cd690999e4
Adela Spirkova <ad.spirkova@gmail.com>
2019-06-07 10:02:58 +0200

    Commiting laravel i, Songs controller

---
+++ app/Http/Controllers/SongsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SongsController extends Controller
+{
+    public function hi()
+    {
+        echo 'Hi there!';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/songs', 'SongsController@hi');
