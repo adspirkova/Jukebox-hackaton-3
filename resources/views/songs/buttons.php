efa3f03291c89dfbd6712de3398f61ef180e52b5
yuri <yuri.haruta@gmail.com>
2019-06-07 16:12:47 +0200

    style of list

--- resources/views/songs/buttons.php
+++ resources/views/songs/buttons.php
@@ -1,6 +1,8 @@
 <h2>Choose the button</h2>
+<hr>
 <button type="button" class="btn btn-primary">Add</button>
 <a href="edit"><button type="button" class="btn btn-warning">Edit</button></a>
 <button type="button" class="btn btn-danger">Delete</button>
 <button type="button" class="btn btn-success">Play</button>
+<br><br>
 
