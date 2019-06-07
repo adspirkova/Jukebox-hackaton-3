2bb5d358d3cc763dde43476e669ba61bb93da7d4
yuri <yuri.haruta@gmail.com>
2019-06-07 11:00:55 +0200

    html wrapper

---
+++ resources/views/songs/buttons.php
@@ -0,0 +1,4 @@
+<button type="button" class="btn btn-primary">Add</button>
+<button type="button" class="btn btn-secondary">Edit</button>
+<button type="button" class="btn btn-success">Delete</button>
+<button type="button" class="btn btn-info">Play</button>
